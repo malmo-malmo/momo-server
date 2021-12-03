1fb1a176714b6aef1a2e7f7e7eb14e053e142ce6
Yoel-18 <63850050+Yoel-18@users.noreply.github.com>
2021-12-04 00:14:06 +0900

    refactor: 앱 바 수정 #127

--- client/momo/lib/app/ui/components/app_bar/custom_app_bar.dart
+++ client/momo/lib/app/ui/components/app_bar/custom_app_bar.dart
@@ -1,32 +1,38 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:momo/app/util/navigation_service.dart';
 import 'package:momo/app/util/theme.dart';
 
-AppBar listAppBar() {
+AppBar customAppBar({
+  required IconData leadingIcon,
+  required bool isAction,
+  String? title,
+  Widget? actionWidget,
+}) {
   return AppBar(
     elevation: 0,
     backgroundColor: const Color(0xffffffff),
+    toolbarHeight: 56,
     leading: Consumer(builder: (context, ref, _) {
       return InkWell(
         onTap: () {
           ref.read(navigatorProvider).pop();
         },
-        child: const Icon(
-          CupertinoIcons.back,
+        child: Icon(
+          leadingIcon,
           color: MomoColor.black,
         ),
       );
     }),
+    title: Text(title ?? '', style: MomoTextStyle.defaultStyle),
     actions: [
-      Padding(
-        padding: const EdgeInsets.only(right: 16),
-        child: SvgPicture.asset(
-          'assets/icon/icon_msg_28.svg',
-        ),
-      ),
+      isAction
+          ? Padding(
+              padding: const EdgeInsets.only(right: 16),
+              child: actionWidget,
+            )
+          : const SizedBox(),
     ],
   );
 }
--- client/momo/lib/app/ui/schedule_list/schedule_list_page.dart
+++ client/momo/lib/app/ui/schedule_list/schedule_list_page.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:momo/app/ui/components/app_bar/list_app_bar.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:momo/app/ui/components/app_bar/custom_app_bar.dart';
 import 'package:momo/app/ui/schedule_list/widget/schedule_list.dart';
 import 'package:momo/app/util/theme.dart';
 
@@ -16,7 +18,11 @@ class ScheduleListPage extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         backgroundColor: MomoColor.backgroundColor,
-        appBar: listAppBar(),
+        appBar: customAppBar(
+          leadingIcon: CupertinoIcons.back,
+          isAction: true,
+          actionWidget: SvgPicture.asset('assets/icon/icon_msg_28.svg'),
+        ),
         body: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16),
           child: Column(

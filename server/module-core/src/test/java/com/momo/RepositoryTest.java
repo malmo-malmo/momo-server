e8fccf4520559e22bdaad744dfcb5dd67c0af2f9
gunkim <gunkim.dev@gmail.com>
2022-01-01 20:41:37 +0900

    test: 공통으로 Repository 테스트에 사용할 메서드 정의

--- server/module-core/src/test/java/com/momo/RepositoryTest.java
+++ server/module-core/src/test/java/com/momo/RepositoryTest.java
@@ -1,9 +1,14 @@
 package com.momo;
 
-import com.momo.TestProfile;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.PersistenceContext;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.TestConfiguration;
@@ -13,9 +18,6 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
 @Disabled
 @DataJpaTest
 @Transactional
@@ -24,11 +26,56 @@ import javax.persistence.PersistenceContext;
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Import(TestConfig.class)
 public class RepositoryTest {
+
+    @Autowired
+    private EntityManagerFactory entityManagerFactory;
+
+    private EntityManager entityManager;
+
+    private EntityTransaction transaction;
+
+    @BeforeEach
+    public void before() {
+        entityManager = entityManagerFactory.createEntityManager();
+        transaction = entityManager.getTransaction();
+    }
+
+    protected <T> T save(T entity) {
+        transaction.begin();
+        try {
+            entityManager.persist(entity);
+            entityManager.flush();
+            transaction.commit();
+            entityManager.clear();
+        } catch (Exception e) {
+            transaction.rollback();
+        }
+        return entity;
+    }
+
+    protected <T> Iterable<T> saveAll(Iterable<T> entities) {
+        transaction.begin();
+
+        for (T entity : entities) {
+            try {
+                entityManager.persist(entity);
+                entityManager.flush();
+                transaction.commit();
+                entityManager.clear();
+            } catch (Exception e) {
+                transaction.rollback();
+            }
+        }
+        return entities;
+    }
 }
+
 @TestConfiguration
 class TestConfig {
+
     @PersistenceContext
     private EntityManager entityManager;
+
     @Bean
     public JPAQueryFactory jpaQueryFactory() {
         return new JPAQueryFactory(entityManager);

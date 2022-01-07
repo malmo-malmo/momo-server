df81dc2da97dddc9fc535ea5ea1b0511cdc771ac
highright96 <nsw5296@naver.com>
2022-01-07 13:55:58 +0900

    test: 관심 모임 등록 API 서비스 테스트 추가 (#218)

---
+++ server/module-core/src/test/java/com/momo/domain/user/service/FavoriteGroupServiceTest.java
@@ -0,0 +1,44 @@
+package com.momo.domain.user.service;
+
+import static java.util.Optional.of;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+import com.momo.common.ServiceTest;
+import com.momo.domain.group.entity.Group;
+import com.momo.domain.group.repository.GroupRepository;
+import com.momo.domain.user.dto.FavoriteGroupCreateRequest;
+import com.momo.domain.user.entity.User;
+import com.momo.domain.user.repository.FavoriteGroupRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+@DisplayName("관심 모임 서비스 테스트")
+public class FavoriteGroupServiceTest extends ServiceTest {
+
+    @Mock
+    private GroupRepository groupRepository;
+
+    @Mock
+    private FavoriteGroupRepository favoriteGroupRepository;
+
+    @InjectMocks
+    FavoriteGroupService favoriteGroupService;
+
+    @Test
+    void 관심_모임으로_등록한다() {
+        FavoriteGroupCreateRequest request = FavoriteGroupCreateRequest.builder().groupId(1L).build();
+        User user = User.builder().id(1L).build();
+        Group group = Group.builder().id(1L).build();
+
+        given(groupRepository.findById(any())).willReturn(of(group));
+
+        favoriteGroupService.create(user, request);
+
+        verify(groupRepository).findById(any());
+        verify(favoriteGroupRepository).save(any());
+    }
+}

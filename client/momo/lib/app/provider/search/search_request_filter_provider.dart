61a583ddb34db62a4399601462b4f92869978f68
Yoel-18 <63850050+Yoel-18@users.noreply.github.com>
2022-01-09 17:29:31 +0900

    refactor: 검색페이지 헬퍼 메서드 수정 #162

--- client/momo/lib/app/provider/search/search_request_filter_provider.dart
+++ client/momo/lib/app/provider/search/search_request_filter_provider.dart
@@ -4,24 +4,38 @@ import 'package:momo/app/provider/category_result_provider.dart';
 import 'package:momo/app/provider/city_result_provider.dart';
 
 final categoryFilterStateProvider = StateNotifierProvider.autoDispose<
-        SearchRequestFilterState, List<CodeCheckPair>>(
-    (ref) => SearchRequestFilterState(categoryCodeNamePair.length));
+    SearchRequestFilterState, List<CodeCheckPair>>(
+  (ref) => SearchRequestFilterState(
+    categoryCodeNamePair.length,
+  ),
+);
 
 final cityFilterStateProvider = StateNotifierProvider.autoDispose<
-        SearchRequestFilterState, List<CodeCheckPair>>(
-    (ref) => SearchRequestFilterState(cityCodeNamePair.length));
+    SearchRequestFilterState, List<CodeCheckPair>>(
+  (ref) => SearchRequestFilterState(
+    cityCodeNamePair.length,
+  ),
+);
 
 class SearchRequestFilterState extends StateNotifier<List<CodeCheckPair>> {
   SearchRequestFilterState(int count)
-      : super(count == categoryCodeNamePair.length
-            ? List.generate(
-                categoryCodeNamePair.length,
-                (index) => CodeCheckPair(
-                    check: false, code: categoryCodeNamePair[index].code))
-            : List.generate(
-                cityCodeNamePair.length,
-                (index) => CodeCheckPair(
-                    check: false, code: cityCodeNamePair[index].name)));
+      : super(
+          count == categoryCodeNamePair.length
+              ? List.generate(
+                  categoryCodeNamePair.length,
+                  (index) => CodeCheckPair(
+                    check: false,
+                    code: categoryCodeNamePair[index].code,
+                  ),
+                )
+              : List.generate(
+                  cityCodeNamePair.length,
+                  (index) => CodeCheckPair(
+                    check: false,
+                    code: cityCodeNamePair[index].name,
+                  ),
+                ),
+        );
 
   void checkFilter(List<bool> checks) {
     state = [

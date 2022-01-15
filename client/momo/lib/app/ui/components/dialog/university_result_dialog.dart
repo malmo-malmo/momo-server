e045ff65d3300186ac3f6ad6f73b73bc3c714b0f
Yoel-18 <63850050+Yoel-18@users.noreply.github.com>
2022-01-15 22:37:51 +0900

    fix: 학교 검색 다이얼로그 수정 #228

--- client/momo/lib/app/ui/components/dialog/university_result_dialog.dart
+++ client/momo/lib/app/ui/components/dialog/university_result_dialog.dart
@@ -19,65 +19,107 @@ class UniversityResultDialog extends ConsumerWidget {
         ref.watch(universityResultProvider(universityName));
     return Dialog(
       insetPadding: const EdgeInsets.all(0),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+      ),
       child: Container(
-        padding: const EdgeInsets.symmetric(vertical: 32),
-        height: 272,
+        padding: const EdgeInsets.only(top: 32),
+        height: 328,
         width: 280,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+        ),
         child: universityResult.when(
           loading: () => const LoadingCard(),
           error: (error, stack) => Center(child: Text(error.toString())),
-          data: (data) => data.isEmpty
-              ? const Center(
-                  child: Text(
-                    '검색한 대학교가 존재하지 않습니다!',
-                  ),
-                )
-              : Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 32),
-                      child: Text('검색결과(${data.length})',
-                          style: MomoTextStyle.subTitle),
-                    ),
-                    const SizedBox(height: 13),
-                    Expanded(
-                      child: Scrollbar(
-                        isAlwaysShown: true,
-                        child: ListView.builder(
-                          itemBuilder: (context, index) {
-                            return InkWell(
-                              onTap: () {
-                                FocusScope.of(context).unfocus();
+          data: (data) {
+            final _checks = ref.watch(universityCheckProvider(data.length));
 
-                                ref
-                                    .read(navigatorProvider)
-                                    .pop(result: data[index]);
-                              },
-                              child: Container(
-                                height: 48,
-                                padding: const EdgeInsets.only(
-                                    left: 32, top: 15, bottom: 15),
-                                color: universityName == data[index]
-                                    ? MomoColor.main
-                                    : MomoColor.flutterWhite,
-                                child: Text(
-                                  data[index],
-                                  style: MomoTextStyle.defaultStyleR.copyWith(
-                                    color: universityName == data[index]
-                                        ? MomoColor.white
-                                        : null,
+            return data.isEmpty
+                ? const Center(child: Text('검색한 대학교가 존재하지 않습니다!'))
+                : Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 32),
+                        child: Text('검색결과(${data.length})',
+                            style: MomoTextStyle.subTitle),
+                      ),
+                      const SizedBox(height: 13),
+                      SizedBox(
+                        height: 192,
+                        child: Scrollbar(
+                          isAlwaysShown: true,
+                          child: ListView.builder(
+                            itemBuilder: (context, index) {
+                              return InkWell(
+                                onTap: () {
+                                  ref
+                                      .read(universityCheckProvider(data.length)
+                                          .notifier)
+                                      .checkOne(index);
+                                },
+                                child: Container(
+                                  height: 48,
+                                  padding: const EdgeInsets.only(
+                                      left: 32, top: 15, bottom: 15),
+                                  color: _checks[index]
+                                      ? MomoColor.main
+                                      : MomoColor.flutterWhite,
+                                  child: Text(
+                                    data[index],
+                                    style: MomoTextStyle.defaultStyleR.copyWith(
+                                      color: _checks[index]
+                                          ? MomoColor.white
+                                          : null,
+                                    ),
                                   ),
                                 ),
-                              ),
-                            );
-                          },
-                          itemCount: data.length,
+                              );
+                            },
+                            itemCount: data.length,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
+                      const Spacer(),
+                      InkWell(
+                        onTap: _checks.indexWhere((element) => element) != -1
+                            ? () {
+                                ref.read(navigatorProvider).pop(
+                                    result: data[_checks
+                                        .indexWhere((element) => element)]);
+                              }
+                            : null,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: const BorderRadius.only(
+                              bottomLeft: Radius.circular(20),
+                              bottomRight: Radius.circular(20),
+                            ),
+                            color:
+                                _checks.indexWhere((element) => element) != -1
+                                    ? MomoColor.main
+                                    : MomoColor.unSelButton,
+                          ),
+                          height: 56,
+                          width: double.infinity,
+                          child: Center(
+                            child: Text(
+                              '확인',
+                              style: MomoTextStyle.defaultStyleR.copyWith(
+                                color:
+                                    _checks.indexWhere((element) => element) !=
+                                            -1
+                                        ? MomoColor.white
+                                        : MomoColor.unSelText,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  );
+          },
         ),
       ),
     );
--- client/momo/lib/app/ui/search/widget/searched_word_cards.dart
+++ client/momo/lib/app/ui/search/widget/searched_word_cards.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:momo/app/theme/theme.dart';
 
 class SearchedWordCards extends StatelessWidget {

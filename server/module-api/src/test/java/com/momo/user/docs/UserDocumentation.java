41399a4199333deaf99e0088993f7ddf2e4c3baf
highright96 <nsw5296@naver.com>
2022-01-16 17:43:00 +0900

    test: 참여 모임 수 조회, 참여 모임 목록 조회 API 문서화 테스트 추가 (#220)

--- server/module-api/src/test/java/com/momo/user/docs/UserDocumentation.java
+++ server/module-api/src/test/java/com/momo/user/docs/UserDocumentation.java
@@ -85,6 +85,35 @@ public class UserDocumentation {
         );
     }
 
+    public static RestDocumentationResultHandler findParticipatingGroupCount() {
+        FieldDescriptor[] response = new FieldDescriptor[]{
+            fieldWithPath("count").type(JsonFieldType.NUMBER).description("참여한 모임 수"),
+        };
+        return document("user/findParticipatingGroupCount",
+            responseFields(response)
+        );
+    }
+
+    public static RestDocumentationResultHandler findParticipatingGroups() {
+        FieldDescriptor[] response = new FieldDescriptor[]{
+            fieldWithPath("[].id").type(JsonFieldType.NUMBER).description("모임 ID"),
+            fieldWithPath("[].name").type(JsonFieldType.STRING).description("모임 이름"),
+            fieldWithPath("[].imageUrl").type(JsonFieldType.STRING)
+                .description("모임 대표 이미지 URL"),
+            fieldWithPath("[].startDate").type(JsonFieldType.STRING)
+                .description("모임 시작 날짜"),
+            fieldWithPath("[].offline").type(JsonFieldType.BOOLEAN)
+                .description("모임 오프라인 여부"),
+            fieldWithPath("[].end").type(JsonFieldType.BOOLEAN)
+                .description("모임 종료 여부"),
+            fieldWithPath("[].participantCnt").type(JsonFieldType.NUMBER)
+                .description("모임 참여자 수"),
+        };
+        return document("user/findParticipatingGroups",
+            responseFields(response)
+        );
+    }
+
     public static RestDocumentationResultHandler update() {
         FieldDescriptor[] requestUser = new FieldDescriptor[]{
             fieldWithPath("nickname").type(JsonFieldType.STRING).description("유저 닉네임"),

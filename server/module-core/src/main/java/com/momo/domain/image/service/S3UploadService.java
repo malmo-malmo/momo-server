414f417a39c6f0cf4cd08a7d9e8b7f32d21a5fc8
gunkim <gunkim.dev@gmail.com>
2022-01-16 21:37:05 +0900

    refactor: s3 이미지 업로드 시에 바로 저장하도록 수정

--- server/module-core/src/main/java/com/momo/domain/image/service/S3UploadService.java
+++ server/module-core/src/main/java/com/momo/domain/image/service/S3UploadService.java
@@ -2,17 +2,18 @@ package com.momo.domain.image.service;
 
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.momo.domain.image.util.ConvertFileUtil;
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import com.google.common.net.MediaType;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @Slf4j
 @Service
@@ -23,17 +24,20 @@ public class S3UploadService implements ImageUploadService {
     private final String awsBucketName;
 
     public S3UploadService(AmazonS3Client amazonS3Client,
-        @Value("${cloud.aws.s3.bucket}") String awsBucketName) {
+                           @Value("${cloud.aws.s3.bucket}") String awsBucketName) {
         this.s3Client = amazonS3Client;
         this.awsBucketName = awsBucketName;
     }
 
     public String upload(MultipartFile multipartFile, String dirName) throws IOException {
-        File uploadFile = ConvertFileUtil.convert(multipartFile);
-        String uploadImageUrl = process(uploadFile, dirName);
-        uploadFile.delete();
-        return uploadImageUrl;
+        String fileName = generateSaveFileName(dirName, multipartFile.getName());
+        ObjectMetadata metadata = generateObjectMeta(multipartFile.getSize());
+
+        s3Client.putObject(new PutObjectRequest(awsBucketName, fileName, multipartFile.getInputStream(), metadata)
+                .withCannedAcl(CannedAccessControlList.PublicRead));
+        return s3Client.getUrl(awsBucketName, fileName).toString();
     }
+
     public List<String> uploadAll(List<MultipartFile> multipartFiles, String dirName) throws IOException {
         List<String> uploadUrls = new ArrayList<>();
         for (MultipartFile multipartFile : multipartFiles) {
@@ -42,16 +46,16 @@ public class S3UploadService implements ImageUploadService {
         }
         return uploadUrls;
     }
-    private String process(File uploadFile, String dirName) {
-        String fileName = generateSaveFileName(dirName, uploadFile.getName());
-        return putS3(uploadFile, fileName);
-    }
+
     private String generateSaveFileName(String dirName, String fileName) {
         return String.format("%s/%s_%s", dirName, UUID.randomUUID(), fileName);
     }
-    private String putS3(File uploadFile, String fileName) {
-        s3Client.putObject(new PutObjectRequest(awsBucketName, fileName, uploadFile)
-            .withCannedAcl(CannedAccessControlList.PublicRead));
-        return s3Client.getUrl(awsBucketName, fileName).toString();
+
+    private ObjectMetadata generateObjectMeta(long fileSize) {
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentType(String.valueOf(MediaType.ANY_IMAGE_TYPE));
+        metadata.setContentLength(fileSize);
+
+        return metadata;
     }
 }

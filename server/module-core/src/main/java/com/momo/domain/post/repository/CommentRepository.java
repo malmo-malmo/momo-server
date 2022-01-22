4148a81a01c1229b3630eeac2882c16b2e442939
gunkim <gunkim.dev@gmail.com>
2022-01-22 16:56:59 +0900

    refactor: 댓글 로직 리팩토링

--- server/module-core/src/main/java/com/momo/domain/post/repository/CommentRepository.java
+++ server/module-core/src/main/java/com/momo/domain/post/repository/CommentRepository.java
@@ -2,13 +2,11 @@ package com.momo.domain.post.repository;
 
 import com.momo.domain.post.entity.Comment;
 import com.momo.domain.post.entity.Post;
-import com.momo.domain.user.entity.User;
-import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
     Page<Comment> findAllByPostOrderByCreatedDateAsc(Post post, Pageable pageable);
-    Optional<Comment> findByIdAndUser(Long commentId, User user);
 }

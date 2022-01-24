a85b9d7cd77726041fccd23f812c09999e857bd1
gunkim <gunkim.dev@gmail.com>
2022-01-24 17:01:25 +0900

    refactor: 요구사항에 의해 게시글, 댓글 시간 표시 형식 수정 및 테스트 코드 작성 (#242)

---
+++ server/module-core/src/main/java/com/momo/domain/common/util/TimeFormatUtil.java
@@ -0,0 +1,46 @@
+package com.momo.domain.common.util;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Objects;
+
+public class TimeFormatUtil {
+
+    private static long SEC = 60;
+    private static long MIN = 60;
+    private static long HOUR = 24;
+    private static long DAY = 30;
+    private static long MONTH = 12;
+
+    public static String generateDateInfo(LocalDateTime localDateTime) {
+        if (Objects.isNull(localDateTime)) {
+            return null;
+        }
+        LocalDateTime now = LocalDateTime.now();
+
+        long diffTime = localDateTime.until(now, ChronoUnit.SECONDS);
+
+        if (diffTime < SEC) {
+            return "방금 전";
+        }
+        diffTime = diffTime / SEC;
+        if (diffTime < MIN) {
+            return diffTime + "분 전";
+        }
+        diffTime = diffTime / MIN;
+        if (diffTime < HOUR) {
+            return diffTime + "시간 전";
+        }
+        diffTime = diffTime / HOUR;
+        if (diffTime < DAY) {
+            return diffTime + "일 전";
+        }
+        diffTime = diffTime / DAY;
+        if (diffTime < MONTH) {
+            return diffTime + "개월 전";
+        }
+
+        diffTime = diffTime / MONTH;
+        return diffTime + "년 전";
+    }
+}

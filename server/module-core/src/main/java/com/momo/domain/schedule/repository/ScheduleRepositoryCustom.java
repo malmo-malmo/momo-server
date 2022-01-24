f2fc329b692f9c0494dbccf5e1519557bc3cc1b3
gunkim <gunkim.dev@gmail.com>
2022-01-25 00:48:25 +0900

    feat: 일정 정보 조회 도메인 로직 구현 및 테스트 코드 작성

--- server/module-core/src/main/java/com/momo/domain/schedule/repository/ScheduleRepositoryCustom.java
+++ server/module-core/src/main/java/com/momo/domain/schedule/repository/ScheduleRepositoryCustom.java
@@ -1,8 +1,8 @@
 package com.momo.domain.schedule.repository;
 
 import com.momo.domain.group.entity.Group;
-import com.momo.domain.schedule.entity.Schedule;
 import com.momo.domain.schedule.dto.GroupScheduleResponse;
+import com.momo.domain.schedule.entity.Schedule;
 import com.momo.domain.user.entity.User;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -11,7 +11,9 @@ import org.springframework.data.domain.Pageable;
 public interface ScheduleRepositoryCustom {
 
     List<GroupScheduleResponse> findAllByGroupAndUserOrderByCreatedDateDesc(Group group, Long userId,
-                                                                            Pageable pageable);
+        Pageable pageable);
 
     List<Schedule> findAllByStartDateTimeBetween(LocalDateTime startDateTime, LocalDateTime endDateTime, User user);
+
+    GroupScheduleResponse findGroupResponseById(Long scheduleId, Long userId);
 }

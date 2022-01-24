738484fbf868318f3bb9ebdf9fb3dcbc2155c474
gunkim <gunkim.dev@gmail.com>
2022-01-24 17:19:53 +0900

    refactor: 게시글 수정, 삭제 API 구현 (#237)

--- server/module-api/src/main/java/com/momo/api/post/PostController.java
+++ server/module-api/src/main/java/com/momo/api/post/PostController.java
@@ -2,6 +2,7 @@ package com.momo.api.post;
 
 import com.momo.common.CurrentUser;
 import com.momo.domain.post.dto.PostCreateRequest;
+import com.momo.domain.post.dto.PostModifyRequest;
 import com.momo.domain.post.dto.PostResponse;
 import com.momo.domain.post.service.PostService;
 import com.momo.domain.user.entity.User;
@@ -11,10 +12,13 @@ import java.net.URISyntaxException;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,5 +40,17 @@ public class PostController {
     public ResponseEntity<PostResponse> find(@CurrentUser User user, @PathVariable Long id) {
         PostResponse postResponse = postService.findById(user, id);
         return ResponseEntity.ok(postResponse);
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> update(@CurrentUser User user, @Valid @RequestBody PostModifyRequest request) {
+        postService.updatePost(request, user);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{postId}")
+    public ResponseEntity<Void> delete(@CurrentUser User user, @PathVariable Long postId) {
+        postService.deletePost(postId, user);
+        return ResponseEntity.ok().build();
     }
 }

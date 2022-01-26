8df4e5826b02ff5577ba1ef885fa5f67895ec8d3
highright96 <nsw5296@naver.com>
2022-01-27 01:08:26 +0900

    feat: 게시물 목록 조회 레포지토리 테스트 추가

--- server/module-core/src/test/java/com/momo/domain/post/repository/PostRepositoryTest.java
+++ server/module-core/src/test/java/com/momo/domain/post/repository/PostRepositoryTest.java
@@ -1,12 +1,14 @@
 package com.momo.domain.post.repository;
 
+import static com.momo.domain.post.entity.PostType.NORMAL;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.data.domain.PageRequest.of;
 
 import com.momo.common.RepositoryTest;
 import com.momo.domain.district.entity.City;
 import com.momo.domain.group.entity.Group;
+import com.momo.domain.post.dto.PostCardResponse;
 import com.momo.domain.post.entity.Post;
-import com.momo.domain.post.entity.PostType;
 import com.momo.domain.user.entity.SocialProvider;
 import com.momo.domain.user.entity.User;
 import java.time.LocalDate;
@@ -16,7 +18,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
 
 @DisplayName("게시글 레포지토리 테스트")
 public class PostRepositoryTest extends RepositoryTest {
@@ -60,7 +61,7 @@ public class PostRepositoryTest extends RepositoryTest {
                 .group(group)
                 .title("테스트 게시글")
                 .contents("테스트 내용")
-                .type(PostType.NORMAL)
+                .type(NORMAL)
                 .build()
         );
     }
@@ -74,7 +75,7 @@ public class PostRepositoryTest extends RepositoryTest {
             () -> assertThat(post.getGroup()).isEqualTo(group),
             () -> assertThat(post.getTitle()).isEqualTo("테스트 게시글"),
             () -> assertThat(post.getContents()).isEqualTo("테스트 내용"),
-            () -> assertThat(post.getType()).isEqualTo(PostType.NORMAL)
+            () -> assertThat(post.getType()).isEqualTo(NORMAL)
         );
     }
 
@@ -86,15 +87,33 @@ public class PostRepositoryTest extends RepositoryTest {
     }
 
     @Test
-    void 로그인한_유저가_올린_게시물을_조회한다() {
+    void 게시글_목록을_조회한다() {
+        List<PostCardResponse> actual = postRepository
+            .findAllWithAuthorByGroupAndTypeOrderByCreatedDateDesc(group, post.getType(), of(0, 10));
+        Assertions.assertAll(
+            () -> assertThat(actual).isNotNull(),
+            () -> assertThat(actual.size()).isEqualTo(1),
+            () -> assertThat(actual.get(0).getId()).isNotNull(),
+            () -> assertThat(actual.get(0).getAuthorImage()).isEqualTo(user.getImageUrl()),
+            () -> assertThat(actual.get(0).getAuthorNickname()).isEqualTo(user.getNickname()),
+            () -> assertThat(actual.get(0).getTitle()).isEqualTo(post.getTitle()),
+            () -> assertThat(actual.get(0).getContents()).isEqualTo(post.getContents()),
+            () -> assertThat(actual.get(0).getCommentCnt()).isEqualTo(0)
+        );
+    }
+
+    @Test
+    void 자신이_올린_게시글을_조회한다() {
         List<Post> actual = postRepository
-            .findAllWithGroupAndAuthorByUserOrderByCreatedDateDesc(user, PageRequest.of(0, 10));
+            .findAllWithGroupAndAuthorByUserAndTypeOrderByCreatedDateDesc(user, NORMAL, of(0, 10));
         Assertions.assertAll(
             () -> assertThat(actual).isNotNull(),
             () -> assertThat(actual.size()).isEqualTo(1),
             () -> assertThat(actual.get(0).getId()).isNotNull(),
             () -> assertThat(actual.get(0).getAuthor().getId()).isEqualTo(user.getId()),
+            () -> assertThat(actual.get(0).getAuthor().getNickname()).isEqualTo(user.getNickname()),
             () -> assertThat(actual.get(0).getGroup().getId()).isEqualTo(group.getId()),
+            () -> assertThat(actual.get(0).getGroup().getName()).isEqualTo(group.getName()),
             () -> assertThat(actual.get(0).getTitle()).isEqualTo(post.getTitle()),
             () -> assertThat(actual.get(0).getContents()).isEqualTo(post.getContents())
         );

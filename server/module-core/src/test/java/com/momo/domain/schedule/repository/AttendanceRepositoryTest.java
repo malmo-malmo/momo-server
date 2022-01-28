f7309bd46c708020a88941f26225c615331061a1
gunkim <gunkim.dev@gmail.com>
2022-01-28 20:02:00 +0900

    test: 출석 레포지토리, 커스텀 메서드에 대한 테스트 코드 작성 (#246, #247)

--- server/module-core/src/test/java/com/momo/domain/schedule/repository/AttendanceRepositoryTest.java
+++ server/module-core/src/test/java/com/momo/domain/schedule/repository/AttendanceRepositoryTest.java
@@ -11,7 +11,9 @@ import com.momo.domain.user.entity.SocialProvider;
 import com.momo.domain.user.entity.User;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,9 +24,15 @@ public class AttendanceRepositoryTest extends RepositoryTest {
     @Autowired
     private AttendanceRepository attendanceRepository;
 
-    @Test
-    public void 출석_정보를_저장한다() {
-        User manager = save(User.builder()
+    private User manager;
+
+    private Group group;
+
+    private Schedule schedule;
+
+    @BeforeEach
+    void setup() {
+        manager = save(User.builder()
             .provider(SocialProvider.KAKAO)
             .providerId("test")
             .refreshToken("refresh Token")
@@ -34,7 +42,7 @@ public class AttendanceRepositoryTest extends RepositoryTest {
             .district("마포구")
             .university("한국대")
             .build());
-        Group group = save(Group.builder()
+        group = save(Group.builder()
             .city(City.SEOUL)
             .district("마포")
             .imageUrl("이미지 주소")
@@ -46,14 +54,13 @@ public class AttendanceRepositoryTest extends RepositoryTest {
             .name("모임 이름")
             .manager(manager)
             .build());
-        Schedule schedule = save(Schedule.builder()
+        schedule = save(Schedule.builder()
             .author(manager)
             .title("오늘의 일정 제목")
             .contents("오늘의 일정")
             .isOffline(false)
             .startDateTime(LocalDateTime.now())
             .build());
-
         attendanceRepository.save(
             Attendance.builder()
                 .group(group)
@@ -62,8 +69,38 @@ public class AttendanceRepositoryTest extends RepositoryTest {
                 .isAttend(false)
                 .build()
         );
+    }
+
+    @Test
+    void 출석_정보를_저장한다() {
         Attendance attendance = attendanceRepository.findAll().get(0);
 
+        Assertions.assertAll(
+            () -> assertThat(attendance.getId()).isNotNull(),
+            () -> assertThat(attendance.getGroup()).isEqualTo(group),
+            () -> assertThat(attendance.getSchedule()).isEqualTo(schedule),
+            () -> assertThat(attendance.getUser()).isEqualTo(manager),
+            () -> assertThat(attendance.isAttend()).isFalse()
+        );
+    }
+    @Test
+    void 해당_유저의_출석_정보를_가져온다() {
+        Attendance attendance = attendanceRepository.findByUserId(manager.getId()).get();
+
+        Assertions.assertAll(
+            () -> assertThat(attendance.getId()).isNotNull(),
+            () -> assertThat(attendance.getGroup()).isEqualTo(group),
+            () -> assertThat(attendance.getSchedule()).isEqualTo(schedule),
+            () -> assertThat(attendance.getUser()).isEqualTo(manager),
+            () -> assertThat(attendance.isAttend()).isFalse()
+        );
+    }
+    @Test
+    void 해당_그룹의_출석_목록을_가져온다() {
+        List<Attendance> attendances = attendanceRepository.findByGroup(group);
+        assertThat(attendances.size()).isEqualTo(1);
+
+        Attendance attendance = attendances.get(0);
         Assertions.assertAll(
             () -> assertThat(attendance.getId()).isNotNull(),
             () -> assertThat(attendance.getGroup()).isEqualTo(group),

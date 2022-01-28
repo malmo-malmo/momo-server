7fdf8b049758ea077015bc3785d0d1b02a85eef7
highright96 <nsw5296@naver.com>
2022-01-28 21:24:21 +0900

    feat: 관리 모임 목록 조회, 그 외 참여 모임 목록 조회 API 컨트롤러 추가 (#252, #253)

--- server/module-api/src/main/java/com/momo/api/management/ManagementController.java
+++ server/module-api/src/main/java/com/momo/api/management/ManagementController.java
@@ -1,11 +1,13 @@
 package com.momo.api.management;
 
 import com.momo.common.CurrentUser;
+import com.momo.domain.management.dto.ManagingGroupCardResponse;
 import com.momo.domain.management.dto.MyPostCardResponse;
 import com.momo.domain.management.dto.ParticipatingGroupCardResponse;
 import com.momo.domain.management.dto.ParticipatingGroupCountResponse;
-import com.momo.domain.user.entity.User;
+import com.momo.domain.management.dto.SummaryParticipationGroupResponse;
 import com.momo.domain.management.service.ManagementService;
+import com.momo.domain.user.entity.User;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -21,15 +23,27 @@ public class ManagementController {
 
     private final ManagementService managementService;
 
-    @GetMapping("/group/participation/count")
+    @GetMapping("/participating-group/count")
     public ResponseEntity<ParticipatingGroupCountResponse> findParticipatingGroupCount(@CurrentUser User user) {
         ParticipatingGroupCountResponse response = managementService.findParticipatingGroupCountByUser(user);
         return ResponseEntity.ok(response);
     }
 
-    @GetMapping("/groups/participation")
+    @GetMapping("/participating-groups/details")
     public ResponseEntity<List<ParticipatingGroupCardResponse>> findParticipatingGroups(@CurrentUser User user) {
         List<ParticipatingGroupCardResponse> responses = managementService.findParticipatingGroupsByUser(user);
+        return ResponseEntity.ok(responses);
+    }
+
+    @GetMapping("/participating-groups/summary")
+    public ResponseEntity<List<SummaryParticipationGroupResponse>> findSummaryParticipationGroups(
+        @CurrentUser User user) {
+        return ResponseEntity.ok(managementService.findSummaryParticipationGroupsByUser(user));
+    }
+
+    @GetMapping("/managing-groups")
+    public ResponseEntity<List<ManagingGroupCardResponse>> findManagingGroups(@CurrentUser User user) {
+        List<ManagingGroupCardResponse> responses = managementService.findManagingGroupsByUser(user);
         return ResponseEntity.ok(responses);
     }
 

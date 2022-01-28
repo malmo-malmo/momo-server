ee5a026064eff25f7a185f04f2a5ab4a8e8a21fd
gunkim <gunkim.dev@gmail.com>
2022-01-28 20:47:55 +0900

    feat: 출석 수정, 조회 API 구현, 컨트롤러 분리 (#246, #247)

--- server/module-core/src/main/java/com/momo/domain/schedule/dto/AttendanceUpdateRequest.java
+++ server/module-core/src/main/java/com/momo/domain/schedule/dto/AttendanceUpdateRequest.java
@@ -1,5 +1,6 @@
 package com.momo.domain.schedule.dto;
 
+import javax.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
@@ -7,11 +8,13 @@ import lombok.ToString;
 @Getter
 @ToString
 public class AttendanceUpdateRequest {
+    @NotNull(message = "출석 ID는 필수 입력 값입니다.")
     private Long attendanceId;
-    private boolean isAttend;
+    @NotNull(message = "출석 여부는 필수 입력 값입니다.")
+    private Boolean isAttend;
 
     @Builder
-    public AttendanceUpdateRequest(Long attendanceId, boolean isAttend) {
+    public AttendanceUpdateRequest(Long attendanceId, Boolean isAttend) {
         this.attendanceId = attendanceId;
         this.isAttend = isAttend;
     }

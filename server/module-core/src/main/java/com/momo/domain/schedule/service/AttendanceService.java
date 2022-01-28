edf5db34a3fb77a1c13331268905c7f348f7a7e8
gunkim <gunkim.dev@gmail.com>
2022-01-28 20:12:36 +0900

    refactor: 서비스에 누락된 메서드 추가 (#247)

--- server/module-core/src/main/java/com/momo/domain/schedule/service/AttendanceService.java
+++ server/module-core/src/main/java/com/momo/domain/schedule/service/AttendanceService.java
@@ -1,12 +1,16 @@
 package com.momo.domain.schedule.service;
 
 import com.momo.domain.schedule.dto.AttendanceCreateRequests;
+import com.momo.domain.schedule.dto.AttendanceResponse;
 import com.momo.domain.schedule.dto.AttendanceUpdateRequest;
 import com.momo.domain.user.entity.User;
+import java.util.List;
 
 public interface AttendanceService {
 
     void create(User user, AttendanceCreateRequests requests);
 
     void update(User user, AttendanceUpdateRequest request);
+
+    List<AttendanceResponse> findGroupAttendances(User user, Long groupId);
 }

531e7cb96fdeeae56f954b4c92ab287bb0e70920
highright96 <nsw5296@naver.com>
2022-01-29 21:12:28 +0900

    test: 관리 모임(요약) 목록 조회 API, 그 외 참여 모임 목록 조회 API 통합/인수 테스트 추가 (#253, #254)

--- server/module-api/src/test/java/com/momo/management/acceptance/ManagementAcceptanceTest.java
+++ server/module-api/src/test/java/com/momo/management/acceptance/ManagementAcceptanceTest.java
@@ -4,20 +4,28 @@ import static com.momo.common.acceptance.step.AcceptanceStep.assertThatStatusIsO
 import static com.momo.fixture.GroupFixture.GROUP_CREATE_REQUEST1;
 import static com.momo.fixture.GroupFixture.GROUP_CREATE_REQUEST2;
 import static com.momo.fixture.UserFixture.getUser1;
+import static com.momo.fixture.UserFixture.getUser2;
 import static com.momo.group.acceptance.step.GroupAcceptanceStep.requestToCreateGroup;
+import static com.momo.group.acceptance.step.ParticipantAcceptanceStep.requestToApplyParticipant;
+import static com.momo.management.acceptance.step.ManagementAcceptanceStep.assertThatFindMyGroupsSummary;
 import static com.momo.management.acceptance.step.ManagementAcceptanceStep.assertThatFindMyPosts;
-import static com.momo.management.acceptance.step.ManagementAcceptanceStep.assertThatfindParticipationGroups;
+import static com.momo.management.acceptance.step.ManagementAcceptanceStep.assertThatFindParticipationGroups;
+import static com.momo.management.acceptance.step.ManagementAcceptanceStep.assertThatFindParticipationGroupsSummary;
+import static com.momo.management.acceptance.step.ManagementAcceptanceStep.requestToFindMyGroupsSummary;
 import static com.momo.management.acceptance.step.ManagementAcceptanceStep.requestToFindMyPosts;
-import static com.momo.management.acceptance.step.ManagementAcceptanceStep.requestTofindParticipationGroupCount;
-import static com.momo.management.acceptance.step.ManagementAcceptanceStep.requestTofindParticipationGroups;
+import static com.momo.management.acceptance.step.ManagementAcceptanceStep.requestToFindParticipationGroupCount;
+import static com.momo.management.acceptance.step.ManagementAcceptanceStep.requestToFindParticipationGroups;
+import static com.momo.management.acceptance.step.ManagementAcceptanceStep.requestToFindParticipationGroupsSummary;
 import static com.momo.post.acceptance.step.PostAcceptanceStep.requestToCreatePost;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.momo.common.acceptance.AcceptanceTest;
 import com.momo.domain.group.dto.GroupResponse;
+import com.momo.domain.management.dto.MyGroupSummaryResponse;
 import com.momo.domain.management.dto.MyPostCardResponse;
 import com.momo.domain.management.dto.ParticipationGroupCardResponse;
 import com.momo.domain.management.dto.ParticipationGroupCountResponse;
+import com.momo.domain.management.dto.ParticipationGroupSummaryResponse;
 import com.momo.domain.post.dto.PostCreateRequest;
 import com.momo.fixture.PostFixture;
 import io.restassured.response.ExtractableResponse;
@@ -34,7 +42,7 @@ public class ManagementAcceptanceTest extends AcceptanceTest {
         String token = getAccessToken(getUser1());
         requestToCreateGroup(token, GROUP_CREATE_REQUEST1);
         requestToCreateGroup(token, GROUP_CREATE_REQUEST2);
-        ExtractableResponse<Response> response = requestTofindParticipationGroupCount(token);
+        ExtractableResponse<Response> response = requestToFindParticipationGroupCount(token);
         Long participationGroupCount = getObject(response, ParticipationGroupCountResponse.class).getCount();
         assertThatStatusIsOk(response);
         assertThat(participationGroupCount).isEqualTo(2);
@@ -44,10 +52,40 @@ public class ManagementAcceptanceTest extends AcceptanceTest {
     void 참여한_모임_목록을_조회한다() {
         String token = getAccessToken(getUser1());
         requestToCreateGroup(token, GROUP_CREATE_REQUEST1);
-        ExtractableResponse<Response> response = requestTofindParticipationGroups(token);
+        ExtractableResponse<Response> response = requestToFindParticipationGroups(token);
         List<ParticipationGroupCardResponse> cardResponses = getObjects(response, ParticipationGroupCardResponse.class);
         assertThatStatusIsOk(response);
-        assertThatfindParticipationGroups(cardResponses, GROUP_CREATE_REQUEST1);
+        assertThatFindParticipationGroups(cardResponses, GROUP_CREATE_REQUEST1);
+    }
+
+    @Test
+    void 그_외_참여한_모임_목록을_조회한다() {
+        String token1 = getAccessToken(getUser1());
+        String token2 = getAccessToken(getUser2());
+        requestToCreateGroup(token1, GROUP_CREATE_REQUEST1);
+        Long groupId = getObject(requestToCreateGroup(token2, GROUP_CREATE_REQUEST2), GroupResponse.class).getId();
+        requestToApplyParticipant(token1, groupId);
+
+        ExtractableResponse<Response> response = requestToFindParticipationGroupsSummary(token1);
+
+        assertThatStatusIsOk(response);
+        assertThatFindParticipationGroupsSummary(
+            getObjects(response, ParticipationGroupSummaryResponse.class), GROUP_CREATE_REQUEST2
+        );
+    }
+
+    @Test
+    void 내_모임_요약_정보_목록을_조회한다() {
+        String token1 = getAccessToken(getUser1());
+        String token2 = getAccessToken(getUser2());
+        requestToCreateGroup(token1, GROUP_CREATE_REQUEST1);
+        Long groupId = getObject(requestToCreateGroup(token2, GROUP_CREATE_REQUEST2), GroupResponse.class).getId();
+        requestToApplyParticipant(token1, groupId);
+
+        ExtractableResponse<Response> response = requestToFindMyGroupsSummary(token1);
+
+        assertThatStatusIsOk(response);
+        assertThatFindMyGroupsSummary(getObjects(response, MyGroupSummaryResponse.class), GROUP_CREATE_REQUEST1);
     }
 
     @Test
--- server/module-api/src/test/java/com/momo/management/acceptance/step/ManagementAcceptanceStep.java
+++ server/module-api/src/test/java/com/momo/management/acceptance/step/ManagementAcceptanceStep.java
@@ -4,8 +4,10 @@ import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.momo.domain.group.dto.GroupCreateRequest;
+import com.momo.domain.management.dto.MyGroupSummaryResponse;
 import com.momo.domain.management.dto.MyPostCardResponse;
 import com.momo.domain.management.dto.ParticipationGroupCardResponse;
+import com.momo.domain.management.dto.ParticipationGroupSummaryResponse;
 import com.momo.domain.post.dto.PostCreateRequest;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
@@ -15,7 +17,7 @@ import org.springframework.http.HttpHeaders;
 
 public class ManagementAcceptanceStep {
 
-    public static void assertThatfindParticipationGroups(List<ParticipationGroupCardResponse> responses,
+    public static void assertThatFindParticipationGroups(List<ParticipationGroupCardResponse> responses,
         GroupCreateRequest request) {
         Assertions.assertAll(
             () -> assertThat(responses.size()).isEqualTo(1),
@@ -26,6 +28,25 @@ public class ManagementAcceptanceStep {
             () -> assertThat(responses.get(0).isOffline()).isEqualTo(request.getIsOffline()),
             () -> assertThat(responses.get(0).isEnd()).isFalse(),
             () -> assertThat(responses.get(0).getParticipantCnt()).isEqualTo(1)
+        );
+    }
+
+    public static void assertThatFindParticipationGroupsSummary(List<ParticipationGroupSummaryResponse> responses,
+        GroupCreateRequest request) {
+        Assertions.assertAll(
+            () -> assertThat(responses.size()).isEqualTo(1),
+            () -> assertThat(responses.get(0).getId()).isNotNull(),
+            () -> assertThat(responses.get(0).getName()).isEqualTo(request.getName()),
+            () -> assertThat(responses.get(0).getCategory()).isEqualTo(request.getCategory())
+        );
+    }
+
+    public static void assertThatFindMyGroupsSummary(List<MyGroupSummaryResponse> responses,
+        GroupCreateRequest request) {
+        Assertions.assertAll(
+            () -> assertThat(responses.size()).isEqualTo(1),
+            () -> assertThat(responses.get(0).getId()).isNotNull(),
+            () -> assertThat(responses.get(0).getName()).isEqualTo(request.getName())
         );
     }
 
@@ -43,20 +64,47 @@ public class ManagementAcceptanceStep {
         );
     }
 
-    public static ExtractableResponse<Response> requestTofindParticipationGroupCount(String token) {
+    public static ExtractableResponse<Response> requestToFindParticipationGroupCount(String token) {
         return given().log().all()
             .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
             .when()
-            .get("/api/management/group/participation/count")
+            .get("/api/management/participation-group/count")
             .then().log().all()
             .extract();
     }
 
-    public static ExtractableResponse<Response> requestTofindParticipationGroups(String token) {
+    public static ExtractableResponse<Response> requestToFindParticipationGroups(String token) {
         return given().log().all()
             .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
             .when()
-            .get("/api/management/groups/participation")
+            .get("/api/management/participation-groups/details")
+            .then().log().all()
+            .extract();
+    }
+
+    public static ExtractableResponse<Response> requestToFindParticipationGroupsSummary(String token) {
+        return given().log().all()
+            .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
+            .when()
+            .get("/api/management/participation-groups/summary")
+            .then().log().all()
+            .extract();
+    }
+
+    public static ExtractableResponse<Response> requestToFindMyGroups(String token) {
+        return given().log().all()
+            .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
+            .when()
+            .get("/api/management/my-groups/details")
+            .then().log().all()
+            .extract();
+    }
+
+    public static ExtractableResponse<Response> requestToFindMyGroupsSummary(String token) {
+        return given().log().all()
+            .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
+            .when()
+            .get("/api/management/my-groups/summary")
             .then().log().all()
             .extract();
     }
@@ -67,7 +115,7 @@ public class ManagementAcceptanceStep {
             .param("page", 0)
             .param("size", 10)
             .when()
-            .get("/api/management/posts")
+            .get("/api/management/my-posts")
             .then().log().all()
             .extract();
     }

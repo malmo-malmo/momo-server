a4926151f806b41b81ccc87f990f8d95c2e04d2a
highright96 <nsw5296@naver.com>
2022-01-29 18:08:40 +0900

    test: 그 외 참여 모임 목록 조회 API 레포지토리 테스트 추가 (#253)

--- server/module-core/src/test/java/com/momo/domain/group/repository/ParticipantRepositoryTest.java
+++ server/module-core/src/test/java/com/momo/domain/group/repository/ParticipantRepositoryTest.java
@@ -3,14 +3,10 @@ package com.momo.domain.group.repository;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.momo.common.RepositoryTest;
-import com.momo.domain.district.entity.City;
-import com.momo.domain.group.entity.Category;
 import com.momo.domain.group.entity.Group;
 import com.momo.domain.group.entity.Participant;
 import com.momo.domain.management.dto.ParticipatingGroupCardResponse;
-import com.momo.domain.user.entity.SocialProvider;
 import com.momo.domain.user.entity.User;
-import java.time.LocalDate;
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -23,79 +19,93 @@ public class ParticipantRepositoryTest extends RepositoryTest {
 
     @Autowired
     private ParticipantRepository participantRepository;
-    private User user;
-    private Group group;
-    private Participant participant;
+
+    private User user1;
+    private Group group1;
+    private Participant participant1;
+
+    private User user2;
+    private Group group2;
+    private Participant participant2;
 
     @BeforeEach
     void before() {
-        user = save(
+        user1 = save(
             User.builder()
-                .provider(SocialProvider.KAKAO)
                 .providerId("test")
-                .refreshToken("refresh Token")
                 .nickname("유저1")
-                .imageUrl("이미지 URL")
-                .city(City.SEOUL)
-                .district("마포구")
-                .university("한국대")
                 .build()
         );
-        group = save(Group.builder()
-            .city(City.SEOUL)
-            .district("마포")
-            .imageUrl("이미지 URL")
-            .introduction("안녕하세요")
-            .university("한국대")
-            .isOffline(false)
-            .isEnd(false)
-            .startDate(LocalDate.now())
-            .name("모임 이름")
-            .category(Category.LIFE)
-            .manager(user)
-            .build());
-        participant = save(
+        group1 = save(
+            Group.builder()
+                .name("모임 이름1")
+                .manager(user1)
+                .build()
+        );
+        participant1 = save(
             Participant.builder()
-                .group(group)
-                .user(user)
+                .group(group1)
+                .user(user1)
+                .build()
+        );
+        user2 = save(
+            User.builder()
+                .providerId("test")
+                .nickname("유저2")
+                .build()
+        );
+        group2 = save(
+            Group.builder()
+                .name("모임 이름2")
+                .manager(user2)
+                .build()
+        );
+        participant2 = save(
+            Participant.builder()
+                .group(group2)
+                .user(user2)
                 .build()
         );
     }
 
     @Test
     void 참가자를_저장한다() {
-        Participant participant = participantRepository.findAll().get(0);
+        Participant actual = participantRepository.findAll().get(0);
         Assertions.assertAll(
-            () -> assertThat(participant).isEqualTo(this.participant),
-            () -> assertThat(participant.getGroup()).isEqualTo(group),
-            () -> assertThat(participant.getUser()).isEqualTo(user)
+            () -> assertThat(actual.getId()).isEqualTo(participant1.getId()),
+            () -> assertThat(actual.getGroup().getId()).isEqualTo(group1.getId()),
+            () -> assertThat(actual.getUser().getId()).isEqualTo(user1.getId())
         );
     }
 
     @Test
     void 모임에_참가한_참가자_목록을_조회한다() {
-        List<Participant> participants = participantRepository.findAllByGroup(group);
-        assertThat(participants.size()).isEqualTo(1);
-        Participant participant = participants.get(0);
-        assertThat(participant).isEqualTo(this.participant);
+        List<Participant> actual = participantRepository.findAllByGroup(group1);
+
+        Assertions.assertAll(
+            () -> assertThat(actual.size()).isEqualTo(1),
+            () -> assertThat(actual.get(0).getId()).isEqualTo(participant1.getId()),
+            () -> assertThat(actual.get(0).getGroup().getId()).isEqualTo(group1.getId()),
+            () -> assertThat(actual.get(0).getId()).isEqualTo(user1.getId())
+        );
     }
 
     @Test
     void 유저가_해당_모임에_참여하고_있는지_조회한다() {
-        boolean isParticipant = participantRepository.existsByGroupAndUser(group, user);
-        assertThat(isParticipant).isTrue();
+        boolean actual = participantRepository.existsByGroupAndUser(group1, user1);
+        assertThat(actual).isTrue();
     }
 
     @Test
     void 유저의_모임_참여를_취소한다() {
-        participantRepository.deleteByGroupAndUser(group, user);
+        participantRepository.deleteByGroupAndUser(group1, user1);
         List<Participant> participants = participantRepository.findAll();
         assertThat(participants.size()).isEqualTo(0);
     }
 
     @Test
     void 유저가_참여한_모임_수를_조회한다() {
-        Long actual = participantRepository.countAllByUser(user);
+        Long actual = participantRepository.countAllByUser(user1);
         assertThat(actual).isEqualTo(1);
     }
 
@@ -103,23 +113,42 @@ public class ParticipantRepositoryTest extends RepositoryTest {
     void 유저가_참여_중인_모임_목록을_조회한다() {
         save(
             Participant.builder()
-                .group(group)
+                .group(group1)
                 .user(save(User.builder().providerId("test").build()))
                 .build()
         );
 
-        List<ParticipatingGroupCardResponse> actual = participantRepository.findParticipatingGroupsByUser(user);
+        List<ParticipatingGroupCardResponse> actual = participantRepository.findParticipatingGroupsByUser(user1);
 
         Assertions.assertAll(
             () -> assertThat(actual).isNotNull(),
             () -> assertThat(actual.size()).isEqualTo(1),
-            () -> assertThat(actual.get(0).getId()).isEqualTo(group.getId()),
-            () -> assertThat(actual.get(0).getName()).isEqualTo(group.getName()),
-            () -> assertThat(actual.get(0).getImageUrl()).isEqualTo(group.getImageUrl()),
-            () -> assertThat(actual.get(0).getStartDate()).isEqualTo(group.getStartDate()),
-            () -> assertThat(actual.get(0).isOffline()).isEqualTo(group.isOffline()),
-            () -> assertThat(actual.get(0).isEnd()).isEqualTo(group.isEnd()),
+            () -> assertThat(actual.get(0).getId()).isEqualTo(group1.getId()),
+            () -> assertThat(actual.get(0).getName()).isEqualTo(group1.getName()),
+            () -> assertThat(actual.get(0).getImageUrl()).isEqualTo(group1.getImageUrl()),
+            () -> assertThat(actual.get(0).getStartDate()).isEqualTo(group1.getStartDate()),
+            () -> assertThat(actual.get(0).isOffline()).isEqualTo(group1.isOffline()),
+            () -> assertThat(actual.get(0).isEnd()).isEqualTo(group1.isEnd()),
             () -> assertThat(actual.get(0).getParticipantCnt()).isEqualTo(2)
+        );
+    }
+
+    @Test
+    void 유저가_모임_관리자가_아닌_참여_중인_모임_목록을_조회한다() {
+        save(
+            Participant.builder()
+                .group(group2)
+                .user(user1)
+                .build()
+        );
+
+        List<Participant> actual = participantRepository
+            .findAllWithGroupByUserAndNotManagerOrderByCreatedDateDesc(user1);
+
+        Assertions.assertAll(
+            () -> assertThat(actual).isNotNull(),
+            () -> assertThat(actual.size()).isEqualTo(1),
+            () -> assertThat(actual.get(0).getGroup().getId()).isEqualTo(group2.getId())
         );
     }
 }

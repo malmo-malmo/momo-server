7a7be1c00eaa1add507f3dc7f925d52a4c2aca3b
highright96 <nsw5296@naver.com>
2022-01-30 23:28:48 +0900

    refactor: 모임 엔티티 리스너, 참여자 엔티티 리스너 core 모듈로 이동 (#255)

--- server/module-core/src/test/java/com/momo/domain/group/entity/GroupListenerTest.java
+++ server/module-core/src/test/java/com/momo/domain/group/entity/GroupListenerTest.java
@@ -1,14 +1,12 @@
-package com.momo.group;
+package com.momo.domain.group.entity;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.momo.common.SpringContainerTest;
+import com.momo.common.RepositoryTest;
 import com.momo.domain.achievementrate.entity.GroupAchievementRate;
 import com.momo.domain.achievementrate.repository.GroupAchievementRateRepository;
-import com.momo.domain.group.entity.Group;
 import com.momo.domain.group.repository.GroupRepository;
 import com.momo.domain.user.entity.User;
-import com.momo.domain.user.repository.UserRepository;
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -17,13 +15,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @DisplayName("모임 리스너 테스트")
-public class GroupListenerTest extends SpringContainerTest {
+public class GroupListenerTest extends RepositoryTest {
 
     @Autowired
     private GroupAchievementRateRepository groupAchievementRateRepository;
-
-    @Autowired
-    private UserRepository userRepository;
 
     @Autowired
     private GroupRepository groupRepository;
@@ -32,13 +27,13 @@ public class GroupListenerTest extends SpringContainerTest {
 
     @BeforeEach
     void before() {
-        User user = userRepository.save(
+        User user = save(
             User.builder()
                 .providerId("test")
                 .nickname("유저")
                 .build()
         );
-        group = groupRepository.save(
+        group = save(
             Group.builder()
                 .name("모임 이름")
                 .manager(user)
@@ -49,7 +44,7 @@ public class GroupListenerTest extends SpringContainerTest {
     @Test
     void 모임을_저장하면_저장_리스너가_실행된다() {
         verifyGroupPersistListener();
-        verifyExistsGroupAchievementRate();
+        verifyExistsGroupAchievementRateField();
     }
 
     private void verifyGroupPersistListener() {
@@ -63,7 +58,7 @@ public class GroupListenerTest extends SpringContainerTest {
         );
     }
 
-    private void verifyExistsGroupAchievementRate() {
+    private void verifyExistsGroupAchievementRateField() {
         Group actual = groupRepository.findById(this.group.getId()).get();
         assertThat(actual.getAchievementRate()).isNotNull();
     }
--- server/module-core/src/test/java/com/momo/domain/group/entity/ParticipantListenerTest.java
+++ server/module-core/src/test/java/com/momo/domain/group/entity/ParticipantListenerTest.java
@@ -1,16 +1,11 @@
-package com.momo.group;
+package com.momo.domain.group.entity;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.momo.common.SpringContainerTest;
+import com.momo.common.RepositoryTest;
 import com.momo.domain.achievementrate.entity.ParticipantAchievementRate;
 import com.momo.domain.achievementrate.repository.ParticipantAchievementRateRepository;
-import com.momo.domain.group.entity.Group;
-import com.momo.domain.group.entity.Participant;
-import com.momo.domain.group.repository.GroupRepository;
-import com.momo.domain.group.repository.ParticipantRepository;
 import com.momo.domain.user.entity.User;
-import com.momo.domain.user.repository.UserRepository;
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -19,37 +14,28 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @DisplayName("참여자 리스너 테스트")
-public class ParticipantListenerTest extends SpringContainerTest {
+public class ParticipantListenerTest extends RepositoryTest {
 
     @Autowired
     private ParticipantAchievementRateRepository participantAchievementRateRepository;
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private GroupRepository groupRepository;
-
-    @Autowired
-    private ParticipantRepository participantRepository;
 
     private Participant participant;
 
     @BeforeEach
     void before() {
-        User user = userRepository.save(
+        User user = save(
             User.builder()
                 .providerId("test")
                 .nickname("유저")
                 .build()
         );
-        Group group = groupRepository.save(
+        Group group = save(
             Group.builder()
                 .name("모임 이름")
                 .manager(user)
                 .build()
         );
-        participant = participantRepository.save(
+        participant = save(
             Participant.builder()
                 .group(group)
                 .user(user)

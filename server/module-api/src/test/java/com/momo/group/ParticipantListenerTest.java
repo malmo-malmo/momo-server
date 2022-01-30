9b8801d41b0ae1e5f5cbf3d458f69c2809e20d60
highright96 <nsw5296@naver.com>
2022-01-30 18:45:48 +0900

    refactor: 모임 엔티티 리스너 테스트와 참여자 엔티티 리스너 테스트 분리 (#255)

--- server/module-api/src/test/java/com/momo/group/ParticipantListenerTest.java
+++ server/module-api/src/test/java/com/momo/group/ParticipantListenerTest.java
@@ -3,10 +3,12 @@ package com.momo.group;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.momo.common.SpringContainerTest;
-import com.momo.domain.achievementrate.entity.GroupAchievementRate;
-import com.momo.domain.achievementrate.repository.GroupAchievementRateRepository;
+import com.momo.domain.achievementrate.entity.ParticipantAchievementRate;
+import com.momo.domain.achievementrate.repository.ParticipantAchievementRateRepository;
 import com.momo.domain.group.entity.Group;
+import com.momo.domain.group.entity.Participant;
 import com.momo.domain.group.repository.GroupRepository;
+import com.momo.domain.group.repository.ParticipantRepository;
 import com.momo.domain.user.entity.User;
 import com.momo.domain.user.repository.UserRepository;
 import java.util.List;
@@ -16,11 +18,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-@DisplayName("모임 리스너 테스트")
-public class GroupListenerTest extends SpringContainerTest {
+@DisplayName("참여자 리스너 테스트")
+public class ParticipantListenerTest extends SpringContainerTest {
 
     @Autowired
-    private GroupAchievementRateRepository groupAchievementRateRepository;
+    private ParticipantAchievementRateRepository participantAchievementRateRepository;
 
     @Autowired
     private UserRepository userRepository;
@@ -28,7 +30,10 @@ public class GroupListenerTest extends SpringContainerTest {
     @Autowired
     private GroupRepository groupRepository;
 
-    private Group group;
+    @Autowired
+    private ParticipantRepository participantRepository;
+
+    private Participant participant;
 
     @BeforeEach
     void before() {
@@ -38,33 +43,33 @@ public class GroupListenerTest extends SpringContainerTest {
                 .nickname("유저")
                 .build()
         );
-        group = groupRepository.save(
+        Group group = groupRepository.save(
             Group.builder()
                 .name("모임 이름")
                 .manager(user)
                 .build()
         );
+        participant = participantRepository.save(
+            Participant.builder()
+                .group(group)
+                .user(user)
+                .build()
+        );
     }
 
     @Test
-    void 모임을_저장하면_저장_리스너가_실행된다() {
-        verifyGroupPersistListener();
-        verifyExistsGroupAchievementRate();
+    void 참여자를_저장하면_저장_리스너가_실행된다() {
+        verifyParticipantPersistListener();
     }
 
-    private void verifyGroupPersistListener() {
-        List<GroupAchievementRate> actual = groupAchievementRateRepository.findAll();
+    private void verifyParticipantPersistListener() {
+        List<ParticipantAchievementRate> actual = participantAchievementRateRepository.findAll();
         Assertions.assertAll(
             () -> assertThat(actual).isNotNull(),
             () -> assertThat(actual.size()).isEqualTo(1),
             () -> assertThat(actual.get(0).getId()).isNotNull(),
-            () -> assertThat(actual.get(0).getGroup().getId()).isEqualTo(group.getId()),
+            () -> assertThat(actual.get(0).getParticipant().getId()).isEqualTo(participant.getId()),
             () -> assertThat(actual.get(0).getRate()).isNotNull()
         );
-    }
-
-    private void verifyExistsGroupAchievementRate() {
-        Group actual = groupRepository.findById(this.group.getId()).get();
-        assertThat(actual.getAchievementRate()).isNotNull();
     }
 }

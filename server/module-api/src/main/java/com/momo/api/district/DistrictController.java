85fdb1a8deb415b727e58bf330931158b951a660
highright96 <nsw5296@naver.com>
2022-02-06 20:07:12 +0900

    refactor: Swagger 관련 코드 제거

--- server/module-api/src/main/java/com/momo/api/district/DistrictController.java
+++ server/module-api/src/main/java/com/momo/api/district/DistrictController.java
@@ -5,7 +5,7 @@ import com.momo.domain.district.dto.DistrictResponse;
 import com.momo.domain.district.entity.City;
 import com.momo.domain.district.entity.District;
 import com.momo.domain.district.repository.DistrictRepository;
-import io.swagger.annotations.ApiOperation;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,21 +13,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class DistrictController {
+
     private final DistrictRepository districtRepository;
 
-    @ApiOperation(value = "시/도 목록 조회")
     @GetMapping("/district/cities")
     public ResponseEntity<List<EnumResponse>> findCities() {
         return ResponseEntity.ok(EnumResponse.listOfCity());
     }
 
-    @ApiOperation(value = "구/군 목록 조회")
     @GetMapping("/district/districts")
     public ResponseEntity<List<DistrictResponse>> findDistrictsByCity(@RequestParam String cityCode) {
         List<District> districts = districtRepository.findAllByCity(City.fromCode(cityCode));
--- server/module-api/src/main/java/com/momo/api/user/UniversityController.java
+++ server/module-api/src/main/java/com/momo/api/user/UniversityController.java
@@ -2,7 +2,6 @@ package com.momo.api.user;
 
 import com.momo.domain.user.dto.UniversityResponse;
 import com.momo.domain.user.service.UniversityService;
-import io.swagger.annotations.ApiOperation;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -18,7 +17,6 @@ public class UniversityController {
 
     private final UniversityService universityService;
 
-    @ApiOperation(value = "대학교 목록 조회")
     @GetMapping("/universities")
     public ResponseEntity<List<UniversityResponse>> findByUniversityName(@RequestParam String universityName) {
         return ResponseEntity.ok(universityService.findByUniversityName(universityName));

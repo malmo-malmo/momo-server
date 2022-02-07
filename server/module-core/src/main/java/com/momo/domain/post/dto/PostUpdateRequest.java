82daac65f8246e4d47108d478dc22a9667f9e181
highright96 <nsw5296@naver.com>
2022-02-07 18:10:21 +0900

    refactor: 게시글 수정 DTO 수정 - content -> contents로 변경

--- server/module-core/src/main/java/com/momo/domain/post/dto/PostUpdateRequest.java
+++ server/module-core/src/main/java/com/momo/domain/post/dto/PostUpdateRequest.java
@@ -5,26 +5,30 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
 @Getter
+@NoArgsConstructor
 public class PostUpdateRequest {
 
     @NotNull(message = "게시글 ID는 필수 입력 값입니다.")
     private Long postId;
+
     @NotBlank(message = "수정할 게시글 제목은 필수 입력 값입니다.")
     private String title;
+
     @NotBlank(message = "수정할 게시글 내용은 필수 입력 값입니다.")
-    private String content;
+    private String contents;
 
     private List<MultipartFile> images;
 
     @Builder
-    public PostUpdateRequest(Long postId, String title, String content,
+    public PostUpdateRequest(Long postId, String title, String contents,
         List<MultipartFile> images) {
         this.postId = postId;
         this.title = title;
-        this.content = content;
+        this.contents = contents;
         this.images = images;
     }
 }

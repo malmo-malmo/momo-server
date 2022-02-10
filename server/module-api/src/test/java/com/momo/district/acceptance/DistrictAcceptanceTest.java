235c0d76014b6fb2185bb88538cc69672e4ddfce
highright96 <nsw5296@naver.com>
2022-02-10 21:21:55 +0900

    refactor: 행정구역 통합/인수 테스트 리팩토링 (#261)

--- server/module-api/src/test/java/com/momo/district/acceptance/DistrictAcceptanceTest.java
+++ server/module-api/src/test/java/com/momo/district/acceptance/DistrictAcceptanceTest.java
@@ -1,18 +1,17 @@
 package com.momo.district.acceptance;
 
+import static com.momo.common.acceptance.step.AcceptanceStep.assertThatStatusIsOk;
 import static com.momo.district.acceptance.step.DistrictAcceptanceStep.assertThatFindCities;
 import static com.momo.district.acceptance.step.DistrictAcceptanceStep.assertThatFindDistricts;
 import static com.momo.district.acceptance.step.DistrictAcceptanceStep.requestToFindCities;
 import static com.momo.district.acceptance.step.DistrictAcceptanceStep.requestToFindDistricts;
 
 import com.momo.common.acceptance.AcceptanceTest;
-import com.momo.common.acceptance.step.AcceptanceStep;
 import com.momo.domain.common.dto.EnumResponse;
 import com.momo.domain.district.dto.DistrictResponse;
 import com.momo.domain.district.entity.City;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
-import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -22,16 +21,16 @@ public class DistrictAcceptanceTest extends AcceptanceTest {
     @Test
     public void 시도_목록을_조회한다() {
         ExtractableResponse<Response> response = requestToFindCities();
-        List<EnumResponse> enumResponses = getObjects(response, EnumResponse.class);
-        AcceptanceStep.assertThatStatusIsOk(response);
-        assertThatFindCities(enumResponses);
+
+        assertThatStatusIsOk(response);
+        assertThatFindCities(getObjects(response, EnumResponse.class));
     }
 
     @Test
     public void 구군_목록을_조회한다() {
         ExtractableResponse<Response> response = requestToFindDistricts(City.SEOUL.getCode());
-        List<DistrictResponse> districtResponses = getObjects(response, DistrictResponse.class);
-        AcceptanceStep.assertThatStatusIsOk(response);
-        assertThatFindDistricts(districtResponses);
+
+        assertThatStatusIsOk(response);
+        assertThatFindDistricts(getObjects(response, DistrictResponse.class));
     }
 }

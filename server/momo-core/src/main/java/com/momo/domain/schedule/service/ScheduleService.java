1220a533ea89702b1655d31e0ef597ccb6911231
highright96 <nsw5296@naver.com>
2022-02-21 21:40:38 +0900

    feat: 다가오는 일정 조회 API 추가 (#277)

--- server/momo-core/src/main/java/com/momo/domain/schedule/service/ScheduleService.java
+++ server/momo-core/src/main/java/com/momo/domain/schedule/service/ScheduleService.java
@@ -4,6 +4,7 @@ import com.momo.domain.schedule.dto.GroupScheduleResponse;
 import com.momo.domain.schedule.dto.GroupScheduleResponses;
 import com.momo.domain.schedule.dto.GroupSchedulesRequest;
 import com.momo.domain.schedule.dto.ScheduleCreateRequest;
+import com.momo.domain.schedule.dto.UpcomingScheduleResponse;
 import com.momo.domain.schedule.dto.UserScheduleResponse;
 import com.momo.domain.schedule.dto.UserSchedulesRequest;
 import com.momo.domain.user.entity.User;
@@ -16,4 +17,6 @@ public interface ScheduleService {
     GroupScheduleResponses findPageByUserAndGroupId(User user, GroupSchedulesRequest request);
 
     List<UserScheduleResponse> findPageByUserAndSearchDate(User user, UserSchedulesRequest request);
+
+    UpcomingScheduleResponse findUpcomingScheduleByGroupId(User user, Long groupId);
 }

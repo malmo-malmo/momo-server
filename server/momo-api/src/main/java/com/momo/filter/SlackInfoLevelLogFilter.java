7cf5a24bd9344ae019dd36642e0b3486e02fc55b
highright96 <nsw5296@naver.com>
2022-02-26 21:16:25 +0900

    feat: 슬랙 로그 필터 생성 (#291)

---
+++ server/momo-api/src/main/java/com/momo/filter/SlackInfoLevelLogFilter.java
@@ -0,0 +1,39 @@
+package com.momo.filter;
+
+import static ch.qos.logback.classic.Level.INFO_INT;
+import static ch.qos.logback.core.spi.FilterReply.DENY;
+import static ch.qos.logback.core.spi.FilterReply.NEUTRAL;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.filter.Filter;
+import ch.qos.logback.core.spi.FilterReply;
+import com.momo.common.ApiLogAspect;
+import com.momo.common.ElapsedTimeLogAspect;
+import java.util.List;
+
+public class SlackInfoLevelLogFilter extends Filter<ILoggingEvent> {
+
+    private final int level;
+    private final List<String> accept;
+
+    public SlackInfoLevelLogFilter() {
+        this.level = INFO_INT;
+        this.accept = List.of(ApiLogAspect.class.getName(), ElapsedTimeLogAspect.class.getName());
+    }
+
+    @Override
+    public FilterReply decide(ILoggingEvent event) {
+        int eventLevel = event.getLevel().levelInt;
+        if (eventLevel != level) {
+            return DENY;
+        }
+        return checkAcceptLogs(event.getLoggerName());
+    }
+
+    private FilterReply checkAcceptLogs(String loggerName) {
+        if (accept.contains(loggerName)) {
+            return NEUTRAL;
+        }
+        return DENY;
+    }
+}

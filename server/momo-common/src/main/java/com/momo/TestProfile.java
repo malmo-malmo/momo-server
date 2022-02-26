45e83b86818aa5a71c952637fc83c7114c8d3aae
highright96 <nsw5296@naver.com>
2022-02-26 21:15:07 +0900

    refactor: 테스트 프로필 생성

--- server/momo-common/src/main/java/com/momo/TestProfile.java
+++ server/momo-common/src/main/java/com/momo/TestProfile.java
@@ -2,6 +2,7 @@ package com.momo;
 
 public class TestProfile {
 
+    public static final String DEVELOP = "dev";
     public static final String LOCAL = "local";
     public static final String TEST = "test";
 }

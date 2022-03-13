b4578f9b1ee031adc6e4e917696ed1a054e805dc
highright96 <nsw5296@naver.com>
2022-03-14 01:26:02 +0900

    feat: Redis 연동 (#301)

---
+++ server/momo-core/src/main/java/com/momo/config/RedisConfig.java
@@ -0,0 +1,37 @@
+package com.momo.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@Configuration
+@EnableRedisRepositories
+public class RedisConfig {
+
+    private final String host;
+    private final int port;
+
+    public RedisConfig(
+        @Value("${redis.datasource.host}") String host,
+        @Value("${redis.datasource.port}") int port
+    ) {
+        this.host = host;
+        this.port = port;
+    }
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(host, port));
+    }
+
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate() {
+        RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        return redisTemplate;
+    }
+}

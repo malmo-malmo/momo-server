71f5437db5d990e3454396ed5a50f1c04e2f33d7
gunkim <gunkim.dev@gmail.com>
2022-03-27 22:47:27 +0900

    feat: 사용자 인가 처리를 위한 설정 & resolver 구현 (#275)

---
+++ server/momo-socket/src/main/java/com/momo/common/auth/CurrentUserConfig.java
@@ -0,0 +1,26 @@
+package com.momo.common.auth;
+
+import com.momo.domain.auth.service.OAuthService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class CurrentUserConfig implements WebMvcConfigurer {
+
+    private final OAuthService oAuthService;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(createAuthenticationPrincipalArgumentResolver());
+    }
+
+    @Bean
+    public CurrentUserArgumentResolver createAuthenticationPrincipalArgumentResolver() {
+        return new CurrentUserArgumentResolver(oAuthService);
+    }
+}

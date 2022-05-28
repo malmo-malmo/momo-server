bbe1877384f33e751bd974604b4df4496d6c7ec1
gunkim <gunkim.dev@gmail.com>
2022-05-28 23:21:59 +0900

    docs: API 변경으로 채팅방 생성 문서 수정

--- server/momo-socket/src/test/java/com/momo/chat/api/docs/document/CreateChatDocumentation.java
+++ server/momo-socket/src/test/java/com/momo/chat/api/docs/document/CreateChatDocumentation.java
@@ -1,10 +1,14 @@
 package com.momo.chat.api.docs.document;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 
 import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.restdocs.payload.FieldDescriptor;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.restdocs.request.ParameterDescriptor;
 
 public class CreateChatDocumentation {
@@ -13,8 +17,12 @@ public class CreateChatDocumentation {
         ParameterDescriptor[] path = new ParameterDescriptor[]{
             parameterWithName("groupId").description("채팅방 ID")
         };
+        FieldDescriptor[] response = new FieldDescriptor[]{
+            fieldWithPath("chatId").type(JsonFieldType.NUMBER).description("생성된 채팅방 ID")
+        };
         return document("chat/createChat",
-            pathParameters(path)
+            pathParameters(path),
+            responseFields(response)
         );
     }
 }
